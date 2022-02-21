9701dbd1960e4a7e264591bbdc447ab97904786c
iloneslife <jasony1@naver.com>
2022-02-21 09:51:35 +0900

    image picker ios 대응 info.plist 수정

--- lib/presentation/photo_upload/photo_upload_screen.dart
+++ lib/presentation/photo_upload/photo_upload_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:firebase_sample/presentation/photo_upload/photo_upload_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PhotoUploadScreen extends StatefulWidget {
   const PhotoUploadScreen({Key? key}) : super(key: key);
@@ -8,8 +12,67 @@ class PhotoUploadScreen extends StatefulWidget {
 }
 
 class _PhotoUploadScreenState extends State<PhotoUploadScreen> {
+  final viewModel = PhotoUploadViewModle();
+  final ImagePicker _picker = ImagePicker();
+
+  XFile? _xFile;
+  // Uint8List? _memory;
+
+  final titleTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('사진 업로드'),
+      ),
+      body: Column(
+        children: [
+          InkWell(
+            onTap: () async {
+              final XFile? image =
+                  await _picker.pickImage(source: ImageSource.gallery);
+              if (image == null) {
+                // 취소
+              } else {
+                final bytes = await image.readAsBytes();
+                setState(() {
+                  _xFile = image;
+                  // _memory = bytes;
+                });
+              }
+            },
+            child: SizedBox(
+              width: double.infinity,
+              height: 300,
+              // child: _memory == null ? Placeholder() : Image.memory(_memory!),
+              child: _xFile == null
+                  ? Placeholder()
+                  : Image.file(File(_xFile!.path)),
+            ),
+          ),
+          TextField(
+            controller: titleTextController,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // if (_memory != null) {
+              //   viewModel.uploadPhoto(_memory., titleTextController.text);
+              // }
+              if (_xFile != null) {
+                viewModel.uploadPhoto(_xFile!.path, titleTextController.text);
+              }
+            },
+            child: const Text('업로드'),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/presentation/photo_upload/photo_upload_view_model.dart
+++ lib/presentation/photo_upload/photo_upload_view_model.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import '../../domain/model/photo.dart';
 
+/*
+  사진 파일을 업로드한다
+ */
 class PhotoUploadViewModle {
   final photoRef =
       FirebaseFirestore.instance.collection('photos').withConverter<Photo>(
@@ -10,11 +17,64 @@ class PhotoUploadViewModle {
           );
 
   // 1. 파일 업로드 : Storage
+  Future<String?> _uploadMemory(String filePath) async {
+    File file = File(filePath);
+
+    try {
+      final refName = 'uploads/${FirebaseAuth.instance.currentUser!.uid}'
+          '-${DateTime.now().millisecondsSinceEpoch}.${filePath.split('.').last}';
+      await FirebaseStorage.instance.ref(refName).putFile(file);
+
+      final downloadUrl = await _getDownloadURL(refName);
+      return downloadUrl;
+    } on FirebaseException catch (e) {
+      return null;
+    }
+  }
+
+  Future<String?> _uploadFile(String filePath) async {
+    File file = File(filePath);
+
+    try {
+      final refName = 'uploads/${FirebaseAuth.instance.currentUser!.uid}'
+          '-${DateTime.now().millisecondsSinceEpoch}.${filePath.split('.').last}';
+      await FirebaseStorage.instance.ref(refName).putFile(file);
+
+      final downloadUrl = await _getDownloadURL(refName);
+      return downloadUrl;
+    } on FirebaseException catch (e) {
+      return null;
+    }
+  }
 
   // 2. 업로드 URL 얻기
+  Future<String> _getDownloadURL(String refName) async {
+    String downloadURL =
+        await FirebaseStorage.instance.ref(refName).getDownloadURL();
+
+    return downloadURL;
+  }
 
   // 3. DB 업로드  : Firestore
+  Future<void> _addTitle(String url, String title) async {
+    try {
+      await photoRef.add(Photo(url: url, title: title));
+    } catch (e) {
+      // 에러 발생
+      print('add title error ${e.toString()}');
+    }
+  }
 
-  //  전체
+  //  사진 업로드 전체
+  Future<void> uploadPhoto(String filePath, String title) async {
+    // storage 파일 업로드 -> 저장 위치 url 얻기
+    final downloadUrl = await _uploadFile(filePath);
 
+    // store DB 작성
+    if (downloadUrl != null) {
+      await _addTitle(downloadUrl, title);
+    } else {
+      // 에러 처리...
+    }
+  }
 }
